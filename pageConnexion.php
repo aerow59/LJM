45597dfc8a467353510be70d8d353de2306e1856
Nicolas <Nicolas@172.16.2.158>
2018-02-08 14:31:22 +0100

    Merge origin/master
    Conflicts:
    	index.php
    rajout des logos
    css ajout fichier plus code page connexion
    suite css connexion

--- pageConnexion.php
+++ pageConnexion.php
@@ -1,11 +1,14 @@
 <form method="POST" action="#">
-    <h2>Connexion</h2>
-    <p>Connectez vous pour pouvoir accéder à vos informations et/ou à moodle.</p>
-    <input type="text" name="identifiantU"><br><br>
-    <input type="password" name="mdpU"><br><br>
-    <div class="aide">
-        <img src="images/cercleViolet.png">Pas encore inscrit ?
-        <img src="images/cercleBleu.png">Mot de passe perdu ?
-        <img src="images/cercleJaune.png">Besoin d'aide ?
+    <link rel="stylesheet" href="connexion.css">
+    <div class="connexion"><h2>Connexion</h2></div>
+    <div class="textco"> <p>Connectez vous pour pouvoir accéder à vos informations et/ou à moodle.</p></div>
+   
+    <div class="Rectangle_arrondi_2"><input type="text" name="identifiantU"><br><br></div>
+     <div class="Rectangle_arrondi_1"> <input type="password" name="mdpU"><br><br></div>
+   
+     <div class="aide">
+     <div class="Ellipse_1">    <img src="images/cercleViolet.png">Pas encore inscrit ?</div>
+       <div class="Ellipse_2"> <img src="images/cercleBleu.png">Mot de passe perdu ?</div>
+        <div class="Ellipse_3"> <img src="images/cercleJaune.png">Besoin d'aide ?</div>
     </div>
 </form>
