9d61175009fd476bbb7a55cd3ea3f44f98bc0830
Jade Degorre <Jade Degorre@172.16.2.154>
2018-02-09 14:24:12 +0100

    ajout bouton valider connexion

--- pageConnexion.php
+++ pageConnexion.php
@@ -2,13 +2,14 @@
     <link rel="stylesheet" href="connexion.css">
     <div class="connexion"><h2>Connexion</h2></div>
     <div class="textco"> <p>Connectez vous pour pouvoir accéder à vos <br> informations et/ou à moodle.</p></div>
-   
-     <input class="Rectangle_arrondi_2" type="text" name="identifiantU"><br><br>
-     <input class="Rectangle_arrondi_1" type="password" name="mdpU"><br><br>
-   
-     <div class="aide">
-     <div class="Ellipse_1">    <img src="images/cercleViolet.png">Pas encore inscrit ?</div>
-       <div class="Ellipse_2"> <img src="images/cercleBleu.png">Mot de passe perdu ?</div>
+    <form method="POST" action="#">
+        <input class="Rectangle_arrondi_2" type="text" name="identifiantU"><br><br>
+        <input class="Rectangle_arrondi_1" type="password" name="mdpU"><br><br>
+        <input type="submit" value="Valider">
+    </form>
+    <div class="aide">
+        <div class="Ellipse_1">    <img src="images/cercleViolet.png">Pas encore inscrit ?</div>
+        <div class="Ellipse_2"> <img src="images/cercleBleu.png">Mot de passe perdu ?</div>
         <div class="Ellipse_3"> <img src="images/cercleJaune.png">Besoin d'aide ?</div>
     </div>
 </form>
