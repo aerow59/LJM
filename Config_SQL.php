20e1d89469acb073205b935a16bd8f8fb46bed4d
romai <romai@DESKTOP-Q4938O6>
2018-02-12 15:39:36 +0100

    Modif bug page formation

--- Config_SQL.php
+++
@@ -1,2 +0,0 @@
-<?php
-$bdd= new PDO('mysql:host=localhost;dbname=jeanmoulin;charset=utf8','root','');
--- formation.php
+++ formation.php
@@ -1,17 +1,6 @@
 <link rel="stylesheet" href="css/formation.css">
 <div class="titreFormation"><h3>FORMATION</h3></div><br><br><br>
-<?php require ('Config_SQL.php'); 
-/*
- * Affiche les 3 types de formation simples
- */
-$requeteAffichTypeFormation = $bdd->prepare("SELECT nomTypeFormation WHERE nomTypeFormation = 'Générales et Technologiques' AND nomTypeformation = 'Professionnelles' "
-        . "AND nomTypeFormation='Supérieurs'");
-$requeteAffichTypeFormation->execute();
-while($donneesAffichTypeFormation = $requeteAffichFormation->fetch())
-{
-    echo $donneesAffichTypeFormation['nomTypeFormation'];?><br><?php
-}
-
+<?php require ('php/configBDD.php'); 
 $requeteAffichFormation =$bdd->prepare("SELECT nomFormation FROM formation");
 $requeteAffichFormation->execute();
 
