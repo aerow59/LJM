53c754347c0c17ff3bdc1346f5a2091a9aa2594f
Nicolas <Nicolas@172.16.2.158>
2018-02-12 15:27:16 +0100

    Merge origin/master
    css connexions
    
    double imput.
    
    bouton valider.
    
    typo
    
    css formation

--- formation.php
+++ formation.php
@@ -1,2 +1,6 @@
 <link rel="stylesheet" href="css/formation.css">
 <div class="titreFormation"><h3>FORMATION</h3></div>
+
+<div class="carre1"> </div>
+<div class="carre2"> </div>
+<div class="carre3"> </div>
