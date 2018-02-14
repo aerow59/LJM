005e1e3b9a4b87c95ce60f2659e8f91399e8386e
Jade Degorre <Jade Degorre@172.16.2.154>
2018-02-14 13:22:24 +0100

    test Ubuntu

--- contact.php
+++ contact.php
@@ -2,6 +2,10 @@
 <form method="contactEnvoi.php" action="POST">
     <br><br><br><br><br>
     <h4>Ce formulaire vous permet de poser vos questions : </h4>
+    <label for="sujet">Entrez votre nom : </label>
+    <input type="text" class="sujet">
     
+    <label for="prenom">Entrez votre prénom : </label>
+    <input type="text" class="prenom">
 </form>
 
