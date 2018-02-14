c8ca58ddfe3e269effab067a1b923df05a75150a
romai <romai@DESKTOP-Q4938O6>
2018-02-14 10:46:20 +0100

    Visuel page formation : - affichage des formations - Ajout des boutons pour en ajouter

--- formation.php
+++ formation.php
@@ -2,47 +2,81 @@
 
 <div class="titreFormation"><h3>FORMATION</h3></div>
 
-<div class="carre1"> </div>
-<div class="carre2"> </div>
-<div class="carre3"> </div>
-
 <div class="PROFESSIONNELLES"> PROFESSIONNELLES </div>
 <div class="GENERALES_ET_TECHNOLOGIQUES"> GENERALES <br> ET TECHNOLOGIQUES </div>
 <div class="trait1"></div>
+
+    <div class="superieur"> 
+        <?php
+        $requeteAffichFormationSup =$bdd->prepare("SELECT nomFormation FROM formation f INNER JOIN typeformation tf ON f.idTypeFormation=tf.idTypeFormation WHERE nomTypeFormation = 'Supérieurs'");
+        $requeteAffichFormationSup->execute();
+
+        while($donneesAffichFormationSup = $requeteAffichFormationSup->fetch())
+        {
+            echo $donneesAffichFormationSup['nomFormation']; ?><br><?php
+        }?>
+        <button>Ajouter une formation Supérieur</button>
+    </div>
+
+    <div class="generalestechno">
+        <?php
+        $requeteAffichFormationGT =$bdd->prepare("SELECT nomFormation FROM formation f INNER JOIN typeformation tf ON f.idTypeFormation=tf.idTypeFormation WHERE nomTypeFormation = 'Générales et technologiques'");
+        $requeteAffichFormationGT->execute();
+
+        while($donneesAffichFormationGT = $requeteAffichFormationGT->fetch())
+        {
+            echo $donneesAffichFormationGT['nomFormation']; ?><br><?php
+        }?>
+        <button>Ajouter une formation Générale et Technologique</button>
+    </div>
+    <div class="pro"> 
+        <?php
+        $requeteAffichFormationPro =$bdd->prepare("SELECT nomFormation FROM formation f INNER JOIN typeformation tf ON f.idTypeFormation=tf.idTypeFormation WHERE nomTypeFormation = 'Professionnelles'");
+        $requeteAffichFormationPro->execute();
+
+        while($donneesAffichFormationPro = $requeteAffichFormationPro->fetch())
+        {
+            echo $donneesAffichFormationPro['nomFormation']; ?><br><?php
+        }?>
+            
+            <button>Ajouter une formation Professionnelles</button>
+    </div>
+
+
 
 <?php require ('php/configBDD.php'); 
     /*
      * Affiche les formations Pro
      */
-    echo "Formation Pro : ";?><br><?php
-    $requeteAffichFormationPro =$bdd->prepare("SELECT nomFormation FROM formation f INNER JOIN typeformation tf ON f.idTypeFormation=tf.idTypeFormation WHERE nomTypeFormation = 'Professionnelles'");
-    $requeteAffichFormationPro->execute();
-
-    while($donneesAffichFormationPro = $requeteAffichFormationPro->fetch())
-    {
-        echo $donneesAffichFormationPro['nomFormation']; ?><br><?php
-    }
+//    echo "Formation Pro : ";?><?php
+//    $requeteAffichFormationPro =$bdd->prepare("SELECT nomFormation FROM formation f INNER JOIN typeformation tf ON f.idTypeFormation=tf.idTypeFormation WHERE nomTypeFormation = 'Professionnelles'");
+//    $requeteAffichFormationPro->execute();
+//
+//    while($donneesAffichFormationPro = $requeteAffichFormationPro->fetch())
+//    {
+//        echo $donneesAffichFormationPro['nomFormation']; ?><?php
+//    }
     
     /*
      *Affiche les formations generales et techno 
      */
-    echo "Formation GT : ";?><br><?php
-    $requeteAffichFormationGT =$bdd->prepare("SELECT nomFormation FROM formation f INNER JOIN typeformation tf ON f.idTypeFormation=tf.idTypeFormation WHERE nomTypeFormation = 'Générales et technologiques'");
-    $requeteAffichFormationGT->execute();
-
-    while($donneesAffichFormationGT = $requeteAffichFormationGT->fetch())
-    {
-        echo $donneesAffichFormationGT['nomFormation']; ?><br><?php
-    }
+//    echo "Formation GT : ";?><?php
+//    $requeteAffichFormationGT =$bdd->prepare("SELECT nomFormation FROM formation f INNER JOIN typeformation tf ON f.idTypeFormation=tf.idTypeFormation WHERE nomTypeFormation = 'Générales et technologiques'");
+//    $requeteAffichFormationGT->execute();
+//
+//    while($donneesAffichFormationGT = $requeteAffichFormationGT->fetch())
+//    {
+//        echo $donneesAffichFormationGT['nomFormation']; ?><?php
+//    }
     
     /*
      *Affiche les formations supérieurs
      */
-    echo "Formation Supérieurs : ";?><br><?php
-    $requeteAffichFormationSup =$bdd->prepare("SELECT nomFormation FROM formation f INNER JOIN typeformation tf ON f.idTypeFormation=tf.idTypeFormation WHERE nomTypeFormation = 'Supérieurs'");
-    $requeteAffichFormationSup->execute();
-
-    while($donneesAffichFormationSup = $requeteAffichFormationSup->fetch())
-    {
-        echo $donneesAffichFormationSup['nomFormation']; ?><br><?php
-    }
+//    echo "Formation Supérieurs : ";?><?php
+//    $requeteAffichFormationSup =$bdd->prepare("SELECT nomFormation FROM formation f INNER JOIN typeformation tf ON f.idTypeFormation=tf.idTypeFormation WHERE nomTypeFormation = 'Supérieurs'");
+//    $requeteAffichFormationSup->execute();
+//
+//    while($donneesAffichFormationSup = $requeteAffichFormationSup->fetch())
+//    {
+//        echo $donneesAffichFormationSup['nomFormation']; ?><?php
+//    }
