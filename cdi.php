95dd2400a2eecfb0dc87c0b2e59d84f2db1559b7
romai <romai@DESKTOP-Q4938O6>
2018-02-15 11:03:35 +0100

    Visuel page cdi : - ajout du formulaire d'upload de video

--- cdi.php
+++ cdi.php
@@ -1,2 +1,13 @@
 <link rel="stylesheet" href="css/cdi.css">
 <div class="titreGreta"><h3>CDI</h3></div>
+<div class="affichContenu">
+    <h3>Ajouter une video</h3>
+    <form method="post" action="reception.php" enctype="multipart/form-data">
+        <label for="icone">Icône du fichier (JPG, PNG ou GIF | max. 15 Ko) :</label><br />
+        <input type="file" name="icone" id="icone" /><br />
+        <input type="submit" name="submit" value="Envoyer" />
+   </form>
+
+
+    
+</div>
