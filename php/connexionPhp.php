a8c3c38de620c927f97756cb56de0007270ae0d1
romai <romai@DESKTOP-Q4938O6>
2018-02-15 12:02:02 +0100

    projet de merde

--- php/connexionPhp.php
+++ php/connexionPhp.php
@@ -5,7 +5,7 @@ session_start();
 $idU = $_POST['identifiantU'];
 $mdpU = $_POST['mdpU'];
 
-$requete = $bdd->prepare("SELECT *"
+$requete = $bdd->prepare("SELECT nomUser,prenomUser,level"
         . " FROM administration a INNER JOIN level l"
         . " WHERE identifiantUser='$idU'"
         . " AND mdpUser='$mdpU' AND a.idLevel=l.idlevel");
@@ -14,7 +14,7 @@ $requete->execute();
 while($donneesReq = $requete->fetch()){
     $_SESSION['nomUser'] = $donneesReq['nomUser'];
     $_SESSION['prenomUser'] = $donneesReq['prenomUser'];
-    $_SESSION['levelUser'] = $donneesReq['nomLevel'];
+    $_SESSION['levelUser'] = $donneesReq['level'];
 }
 
 header('Location: ../index.php');
