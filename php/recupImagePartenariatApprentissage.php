65d0db154b5aa22cfffefff8b224aef777a8a0e7
romai <romai@DESKTOP-Q4938O6>
2018-02-21 11:20:27 +0100

    Modif affichage en fonction du level de la partie image partenariat

--- php/recupImagePartenariatApprentissage.php
+++ php/recupImagePartenariatApprentissage.php
@@ -4,38 +4,74 @@ require('configBDD.php');
 $requeteAffichImagePart = $bdd->prepare('SELECT nomMultimediaApp FROM multimediapartenariatapprentissage ');
 $requeteAffichImagePart->execute();
 
-$nbRowsDiv1 = $requeteAffichImagePart->rowCount();
 ?><?php
-    if($nbRowsDiv1 == 0){
-        if(isset($_SESSION['levelUser'])){
-            if($_SESSION['levelUser']>=3){
+        if(isset($_SESSION['levelUser']))
+        {
+            if($_SESSION['levelUser']>=3)
+            {
                 ?>
-<div class="formImagePartenariat">
-    <form method="POST" action="php/imagePart.php" enctype="multipart/form-data">
-        <label for="nomEntreprise">Nom de l'entreprise : </label><input type="text" name="nomEntreprise" class="URL">
-        <input type="file" name="image" class="uploadI" value="video"><br>
-        <input type="submit" name="Valider" class="btnValiderV">
-    </form>             
-</div>
+                <div class="formImagePartenariat">
+                    <form method="POST" action="php/imagePart.php" enctype="multipart/form-data">
+                        <table>
+                            <tr>
+                                <td><label for="nomEntreprise">Nom de l'entreprise : </label></td>
+                                <td><input type="text" name="nomEntreprise" class="URL"></td>
+                            </tr>
+                            <tr>
+                                <td><input type="file" name="image" class="uploadI" value="video"></td> 
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td><input type="submit" name="Valider" class="btnValiderV"></td>
+                            </tr>
+                        </table>
+                    </form>             
+                </div>
+                <div class="affichImage"><?php
+                    while($donnees = $requeteAffichImagePart->fetch())
+                    {
+                        $cheminImage = str_replace("../", "", $donnees['nomMultimediaApp']);
+                        ?><img src="<?php echo $cheminImage ?>" width="60%" height="auto"/>
+                            <?php
+                            if(isset($_SESSION['levelUser']))
+                            {
+                                if($_SESSION['levelUser']>=3)
+                                {
+                                    ?>
+                                    <form method="POST" action="php/supprimerImagePartenariat.php">
+                                        <input type="submit" value="Supprimer l'image">
+                                    </form>
+                            <?php
+                                }
+                            }   
+                    }?>
+            </div>
                 <?php
             }
-        }else{
-            ?><h3>Aucune vidéo n'est disponible</h3><?php
         }
-    }else{
-        while($donnees = $requeteAffichImagePart->fetch()){
-            $cheminImage = str_replace("../", "", $donnees['nomMultimediaApp']);
-            ?><img src="<?php echo $cheminImage ?>"/>
-                <?php
-                if(isset($_SESSION['levelUser'])){
-                    if($_SESSION['levelUser']>=3){
-                        ?>
-                        <form method="POST" action="php/supprimerImagePartenariat.php">
-                            <input type="submit" value="Supprimer l'image">
-                        </form><?php
-                    }
-                }   
+        else
+        {?>
+          <div class="affichImage"><?php
+            while($donnees = $requeteAffichImagePart->fetch())
+            {
+                $cheminImage = str_replace("../", "", $donnees['nomMultimediaApp']);
+                ?><img src="<?php echo $cheminImage ?>" width="60%" height="auto"/>
+                    <?php
+                    if(isset($_SESSION['levelUser'])){
+                        if($_SESSION['levelUser']>=3){
+                            ?>
+                            <form method="POST" action="php/supprimerImagePartenariat.php">
+                                <input type="submit" value="Supprimer l'image">
+                            </form><?php
+                        }
+                    }   
+            }?>
+        </div><?php  
         }
-    }
+        ?>
+
+<?php
+    
 
 
