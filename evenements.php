47cc2365d6a8c44dd0f63c3f88472a52577113e4
Nicolas <Nicolas@172.16.2.158>
2018-02-21 11:38:59 +0100

    Merge origin/master
    Conflicts:
    	pageConnexion.php
    e
    
    amelioration css page intro
    mediaQ
    connexion aussi.
    
    EVENEMENT CSS

--- evenements.php
+++ evenements.php
@@ -1,7 +1,11 @@
+<link rel="stylesheet" href="css/Evenements.css">
 <div class="textexpo">
-    <h1>Exposition</span></h1>
+    <h3>Evenement</span></h3>
     <p>Du 07 au 13 août 2018 | </p>
     <h2><br></h2>
     <p>Venez découvrir comment nos ancêtres ont explorés notre système solaire, les fabuleuses avancées technologiques qui ont permis  d'accomplir cet exploit remarquable pour l'epoque.<br><br>Plongez dans le quotidien difficile des hommes et des femmes qui ont faconnés notre présent et à qui nous devons le monde tel qu'il est aujourd'hui.<br><br>Isaac vous permet d'explorer le système solaire et de poser le pied sur des mondes fascinants pour la première fois dans l'histoire de l'humantié et vous offre la possiblité de rendre un vibrant hommage aux pionniers qui ont permis d'écrire cette fabuleuse histoire aujourd'hui, avec vous.
     </p>                
 </div>
+<div class="PRECEDENT">PRECEDENT</div>
+<div class="suivant">SUIVANT</div>
+<img src="images/evenement.png" class="image"> 
