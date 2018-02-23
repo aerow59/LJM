4558f94bf84b0cc09f6df1117120e1a9e7a69375
romai <romai@DESKTOP-Q4938O6>
2018-02-23 14:23:07 +0100

    Modif form livre d'or

--- php/gestionLivreOr.php
+++ php/gestionLivreOr.php
@@ -15,17 +15,26 @@
 
 
 
-header("Content-Type: text/plain"); // Utilisation d'un header pour spécifier le type de contenu de la page. Ici, il s'agit juste de texte brut (text/plain). 
+//header("Content-Type: text/plain"); // Utilisation d'un header pour spécifier le type de contenu de la page. Ici, il s'agit juste de texte brut (text/plain). 
+//
+//$nomPersonne = (isset($_POST["variableNom"])) ? $_GET["variable1"] : NULL;
+////$variable2 = (isset($_GET["variable2"])) ? $_GET["variable2"] : NULL;
+//
+//if ($variable1 && $variable2) {
+//	// Faire quelque chose...
+//	echo "OK";
+//} else {
+//	echo "FAIL";
+//}
 
-$nomPersonne = (isset($_POST["variableNom"])) ? $_GET["variable1"] : NULL;
-//$variable2 = (isset($_GET["variable2"])) ? $_GET["variable2"] : NULL;
 
-if ($variable1 && $variable2) {
-	// Faire quelque chose...
-	echo "OK";
-} else {
-	echo "FAIL";
-}
+$nomPersonne=$_POST['variableNom'];
+$prenomPersonne=$_POST['variablePrenom'];
+$messagePersonne=$_POST['variableMessage'];
+$niveauPersonne=$_POST['variableNiv'];
+
+
+$requeteEnvoieDonneesLivreOr=$bdd->prepare('INSERT INTO livreor')
 
 ?>
 
