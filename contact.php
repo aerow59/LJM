2d638fcd854cf64da2d73a5113d45f73e6ce119e
WaKeNighTt <31860071+WaKeNighTt@users.noreply.github.com>
2018-03-01 00:04:06 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,15 +4,19 @@
     </div>
     <form method="POST" action="php/contactEnvoie.php">
         <br><br><br><br><br>
-        <h4>Ce formulaire vous permet de poser vos questions : </h4><br>
+        <h4>Pour toutes éventuelles remarques ou questions, vous pouvez nous contacter via ce petit formulaire en énoncant vos idées.</h4><br>
+         Civilité : 
+		<input type="radio" name="civi2" value="Mme" /> Madame
+		<input type="radio" name="civi2" value="Mlle" /> Mademoiselle
+		<input type="radio" name="civi2" value="Mr" /> Monsieur <br>
         
-        <label for="prenom">Entrez votre prénom : </label>
-        <input type="text" class="prenom" name="prenom" placeholder="Prenom" id="FIRSTNAME"><br><br>
-        
-        <label for="nom">Entrez votre nom : </label>
+        <label for="nom">Nom</label>
         <input type="text" class="nom" name="nom" placeholder="Nom" id="name"><br><br>
+		
+		<label for="prenom">Prénom</label>
+        <input type="text" class="prenom" name="prenom" placeholder="Prenom" id="FIRSTNAME"><br><br>
 
-        <label for="email">Entrez votre email : </label>
+        <label for="email">Mail</label>
         <input type="text" class="email" name="email" placeholder="Email" id="email"><br><br>
     
         <textarea name="message" rows="10" cols="50" placeholder="Entrez votre message ici."></textarea><br><br>
