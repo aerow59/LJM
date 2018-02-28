53bcaee93698a8b891e6e428cb8695942fb8d3a5
WaKeNighTt <31860071+WaKeNighTt@users.noreply.github.com>
2018-02-28 22:00:30 +0100

    Update pageInformation.php

--- pageInformation.php
+++ pageInformation.php
@@ -1,21 +1,35 @@
     <div class="qui">
         <h3>QUI SOMMES NOUS?</h3>
-        <p>
-            Depuis l'aube de l'humanité l'être humain n'a de cesse de lever les yeux vers le ciel en quête de réponses. cet emmerveillement pour la voute céleste à traversé les âges et nourris de nombreux espoirs. nos ancêtres ont toujours redoublés d'imagination pour observer et comprendre cet espace qui leur paraissait inaccessible et mystérieux.<br><br>
-            Aujourd'hui ce rêve est à portée de main grâce aux technologies dévellopées chez isaac. nous offrons à l'humanité la possibilité de dépasser sa propre imagination et d'enfin considérer l'espace comme un endroit accessible à tous dont les infinités de possiblités sont encore à découvrir.<br><br>                    </p>
+        <p><FONT size="1">
+            Situé à quelques pas du centre de Roubaix, ville de 98.000 habitants, le lycée polyvalent Jean Moulin accueille actuellement un peu plus de 1.000 élèves et étudiants. <br>
+			Il affiche depuis de nombreuses années une identité de lycée formant aux métiers des services, tant en formation initiale (scolaire ou par apprentissage), qu’en formation continue.<br>
+			Environ un tiers de ses élèves sont préparés à différents Bac Professionnels tertiaires, un autre tiers à des Bacs généraux et technologiques, et un peu plus de 350 étudiants et apprentis à des BTS également tertiaires.<br>
+			Dynamique, il enrichit et développe ses formations en collaborant avec d’autres établissements : le lycée Turgot pour les métiers de l’accueil, du commerce et de la vente, <br>
+			le lycée Colbert et l’ESAAT (Ecole Supérieure des Arts Appliqués et du Textile) pour les métiers du web et du e-commerce, le lycée Vertes Feuilles et l’Ecole Nationale de Police pour les métiers de la sécurité.<br>
+			Il mène en outre de nombreux partenariats  avec des établissements d’enseignement supérieur, avec l’ESPEME (du groupe EDHEC), ou avec Sciences Po Paris, qui rendent possibles pour ses élèves <br>
+			des parcours d’excellence diversifiés.<br>
+			Enfin, attentif aux besoins du marché de l’emploi, il innove et crée de nouvelles formations : des formations Bac+1 axées sur les métiers de la sécurité, une  formation Bac+3 dédiée au e-commerce.<br>
+			Depuis quelques années, les élèves et tous les membres de la communauté éducative sont rassemblés autour d’un projet, Ensemble pour réussir. Ce slogan marque la volonté de l’établissement de mettre tout en œuvre <br>
+			pour que chaque élève qui s’engage activement puisse aller au terme de son propre projet. <br>
+			Au-delà de cette volonté, c’est aussi l’expérience, le savoir-faire des équipes pédagogiques et de tous les personnels qui font du lycée Jean Moulin un établissement reconnu pour son haut niveau de performance.<br>
+			Chaque élève, chaque étudiant investi dans son projet peut ainsi, quels que soient son niveau et ses difficultés, oser la réussite.
+			<br><br>                    </FONT></p>
     </div>    
 <div class="descriptions">
         <div class="engine">
             <h4>COMMENT VENIR ?</h4>
                 <div class="enginedes">
                     <img src="" alt="">
-                    <p>Voici le moteur qui a permis l'exploration du systeme solaire a 50% de la vitesse de la lumière.<br><br></p>
+                    <p>Réseau de transport en commun Transpole : 
+							- Métro : Ligne 2, vers St Philibert, Arrêt "Roubaix - Charles de Gaulle" ;<br>
+							- Tramway : Ligne Roubaix-Lille, vers Lille, Arrêt "Jean Moulin" ;<br>
+							- Train : SNCF, Arrêt "Gare Jean Lebas".<br><br></p>
                 </div>
         </div>
         <div class="skyjet">
             <h5>PLAN DU LYCEE</h5>
                 <div class="skyjetdes">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2527.8873179015945!2d3.171414215423497!3d50.68491657950842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c328f3d00d06c1%3A0xb4295bc612cfa9be!2sLyc%C3%A9e+Jean+Moulin+de+Roubaix!5e0!3m2!1sfr!2sfr!4v1517924232213" width="600" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>                        
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2527.8873179015945!2d3.171414215423497!3d50.68491657950842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c328f3d00d06c1%3A0xb4295bc612cfa9be!2sLyc%C3%A9e+Jean+Moulin+de+Roubaix!5e0!3m2!1sfr!2sfr!4v1517924232213" width="600" height="273" frameborder="0" style="border:0" allowfullscreen></iframe>                        
                 </div>
         </div>
 </div> 
