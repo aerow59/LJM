9f25c58c558223e96123bdb129bece9f24b47644
WaKeNighTt <31860071+WaKeNighTt@users.noreply.github.com>
2018-02-28 21:41:13 +0100

    Update evenements.php

--- evenements.php
+++ evenements.php
@@ -1,9 +1,11 @@
 <link rel="stylesheet" href="css/Evenements.css">
 <div class="textexpo">
-    <h3>Evenement</span></h3>
-    <p>Du 07 au 13 août 2018 | </p>
+    <h3>Evènement</span></h3>
+    <p>Le 27 Janvier 2018 </p>
     <h2><br></h2>
-    <p>Venez découvrir comment nos ancêtres ont explorés notre système solaire, les fabuleuses avancées technologiques qui ont permis  d'accomplir cet exploit remarquable pour l'epoque.<br><br>Plongez dans le quotidien difficile des hommes et des femmes qui ont faconnés notre présent et à qui nous devons le monde tel qu'il est aujourd'hui.<br><br>Isaac vous permet d'explorer le système solaire et de poser le pied sur des mondes fascinants pour la première fois dans l'histoire de l'humantié et vous offre la possiblité de rendre un vibrant hommage aux pionniers qui ont permis d'écrire cette fabuleuse histoire aujourd'hui, avec vous.
+    <p>Venez découvrir le lycée Jean Moulin ainsi que ses élèves le 27 janvier 2018 lors des Journées Portes Ouvertes ! <br><br>
+	   De nombreux stands prévus, ainsi que des rencontres avec des élèves & enseignants du lycée. <br><br>
+	   Venez trouver la formation qui vous correspond !
     </p>                
 </div>
 <div class="PRECEDENT">PRECEDENT</div>
