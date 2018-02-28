23a60bc48d5a644251291b4370ba7cc942b5ec43
WaKeNighTt <31860071+WaKeNighTt@users.noreply.github.com>
2018-03-01 00:20:22 +0100

    Update contactEnvoie.php

--- php/contactEnvoie.php
+++ php/contactEnvoie.php
@@ -1,20 +1,29 @@
 <?php
+	
+	/*
+     * Verification de tous les champs
+     */
+	if($_POST['FIRSTNAME']=='' & $_POST['name']=='' & $_POST['mail']==''){
+        echo '<div class="error" >Merci de compléter entièrement votre formulaire.</div>';        
+		exit();
+    }
 
     /*
      * Verification du prenom
      */
 
     if($_POST['FIRSTNAME']==''){
-        echo '<div class="error" >prenom vide </div>';        
+        echo '<div class="error" >Vous n\'avez pas compléter votre prénom.</div>';        
     }
+	
     elseif (strlen($_POST['FIRSTNAME'])<2) 
     {
-        echo '<div class="error" >Votre prenom n\'est pas valide (trop court) ! </div>';
+        echo '<div class="error" >Votre prénom n\'est pas valide (trop court) ! </div>';
         //exit();
     }
     elseif (!preg_match("~^[\w\-\sàäâéèëêçîï]+$~", $_POST['FIRSTNAME']))
     {
-        echo '<div class="error" >Votre prenom n\'est pas valide ! </div>';
+        echo '<div class="error" >Votre prénom n\'est pas valide. (Attention aux accents) </div>';
         //exit();
     }
     else{
@@ -22,22 +31,21 @@
     }
     
     
-    
     /*
      * Verification du nom
      */
 
     if($_POST['name']==''){
-        echo '<div class="error" >Vous devez remplir votre nom ! </div>';
+        echo '<div class="error" >Vous n\'avez pas compléter votre nom.</div>';
         //exit();
     }
     elseif (strlen($_POST['name'])<2) 
     {
-        echo '<div class="error" >Votre nom n\'est pas valide ! </div>';
+        echo '<div class="error" >Votre nom n\'est pas valide (trop court) ! </div>';
         //exit();
     }
     elseif( !preg_match("~^[\w\-\sàäâéèëêçîï]+$~", $_POST['name'])){
-        echo '<div class="error" >Votre nom n\'est pas valide ! </div>';
+        echo '<div class="error" >Votre nom n\'est pas valide. (Attention aux accents)</div>';
         //exit();
     }
     else{
@@ -49,7 +57,7 @@
      */
 
     if($_POST['mail']==''){
-        echo '<div class="error" >Veuillez complétez votre email ! </div>';
+        echo '<div class="error" >Vous n\'avez pas compléter votre email.</div>';
         //exit();
     }
     elseif (!filter_var($_POST['mail'],FILTER_VALIDATE_EMAIL)) {
@@ -65,21 +73,21 @@
      */
 
     if($_POST['IDMESSAGE']==''){
-        echo '<div class="error" >Votre message ne peut pas être vide ! </div>';
+        echo '<div class="error" >Merci de bien compléter votre message.</div>';
         //exit();
     }
-    elseif (strlen($_POST['IDMESSAGE'])<50) 
+    /*elseif (strlen($_POST['IDMESSAGE'])<50) 
     {
         echo '<div class="error" >Votre message n\'est pas assez long (Min 50 caractères) ! </div>';
         //exit();
-    }
+    }*/
     else
     {
         $message=$_POST['IDMESSAGE'];
     }
     
-if($_POST['name']!=='' && $_POST['FIRSTNAME']!=='' && $_POST['mail']!=='' && $_POST['IDMESSAGE']!==''){
-    $to = 'romainbouchez62@gmail.com';
+if($_POST['IDMESSAGE']=='' && $_POST['name']!=='' && strlen($_POST['name'])>=2 && preg_match("~^[\w\-\sàäâéèëêçîï]+$~", $_POST['name']) && $_POST['FIRSTNAME']!=='' && strlen($_POST['FIRSTNAME'])>=2 && preg_match("~^[\w\-\sàäâéèëêçîï]+$~", $_POST['FIRSTNAME']) && $_POST['mail']!=='' && $_POST['IDMESSAGE']!=='' && filter_var($_POST['mail'],FILTER_VALIDATE_EMAIL)){
+    $to = 'franckhannedouche1@gmail.com';
     
     $header="Mime-VErsion 1.0\r\n";
     $header.='Content-type: text/html; charset="utf-8"' . "\n";
@@ -91,4 +99,5 @@ if($_POST['name']!=='' && $_POST['FIRSTNAME']!=='' && $_POST['mail']!=='' && $_P
     
     echo '<div class="success"><h4> Mail envoyé !</h4><br> Merci '.$_POST['name'].' pour votre message.</div>';
 }
+    
     
